f709441badeee25a5c8ee590a9e998a04d89848e
anjali-sim <anjalighetia123@gmail.com>
2023-06-07 18:11:51 +0530

    :wrench: Updated the files

--- src/container/Container.jsx
+++ src/container/Container.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ContainerStyle from '../styled/Container'
+import React from "react";
+import ContainerStyle from "../styled/Container";
 
 function Container(props) {
-  return (
-    <ContainerStyle>{props.children}</ContainerStyle>
-  )
+  return <ContainerStyle>{props.children}</ContainerStyle>;
 }
 
 export default Container;
